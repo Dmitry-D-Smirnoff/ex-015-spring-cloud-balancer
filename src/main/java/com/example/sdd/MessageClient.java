c699238647a7088d453346a75598f825c695c159
Смирнов Дмитрий Дмитриевич <d.smirnov@voskhod.ru>
2021-08-08 19:01:47 +0300

    +

--- src/main/java/com/example/sdd/MessageClient.java
+++ src/main/java/com/example/sdd/MessageClient.java
@@ -1,4 +1,34 @@
 package com.example.sdd;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
 public class MessageClient {
+
+    private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
+
+    MessageClient(RestTemplate restTemplate, DiscoveryClient discoveryClient) {
+        this.restTemplate = restTemplate;
+        this.discoveryClient = discoveryClient;
+    }
+
+    public String getRestTemplateGreeting(String name) {
+        return restTemplate.getForObject("http://ex-014-spring-cloud-client/hello-worlds/" + name, String.class);
+    }
+
+    public String getDiscoveryClientGreeting(String name) {
+
+        ServiceInstance instance = discoveryClient.getInstances("ex-014-spring-cloud-client")
+                .stream().findAny()
+                .orElseThrow(() -> new IllegalStateException("!!! ex-014-spring-cloud-client service unavailable"));
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromHttpUrl(instance.getUri().toString() + "/hello-worlds/" + name);
+        return restTemplate.getForObject("http://ex-014-spring-cloud-client/hello-worlds/" + name, String.class);
+    }
 }
--- src/main/java/com/example/sdd/controller/HelloWorldController.java
+++ src/main/java/com/example/sdd/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.sdd.controller;
 
+import com.example.sdd.MessageClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    private final MessageClient messageClient;
+
     @Value("${greeting}")
     private String currentGreeting;
 
+    public HelloWorldController(MessageClient messageClient) {
+        this.messageClient = messageClient;
+    }
+
     @GetMapping("/hello-worlds/{name}")
     public String getHelloWorld(@PathVariable String name) {
-        return "Hello World " + name + ". My message to You is " + currentGreeting;
+        return "Hello from Balancer. My message to You is " + currentGreeting + ". Embedded message is^ "
+                + messageClient.getRestTemplateGreeting(name);
     }
 }
